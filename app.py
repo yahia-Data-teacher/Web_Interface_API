596e03aedd22f95bef00b09074abd123e8cee452
yahia-m <yhmaamar@gmail.com>
2022-12-15 00:38:17 +0100

    useless requirements removedhis

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return "Hello"
-
+from application import app
 
 
 if __name__== "__main__":
-    app.run(debug=True)
+    app.run()
